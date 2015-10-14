a30382338b2c0dbfdd24cd2d184f0e500e20202e
Bruno V Werneck <brunovaulawerneck@gmail.com>
2015-10-14 14:20:52 -0300

    Correções

---
+++ src/SON/Conta/Types/ContaPremiumType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SON\Conta\Types;
+
+use SON\Conta\ContaAbstract;
+
+class ContaPremiumType extends ContaAbstract
+{
+    public function __construct()
+    {
+        $this->saldo += 10;
+    }
+
+    protected function calculoDeposito($valor)
+    {
+        return $valor += 20;
+    }
+}
